95e123b55125926ebc8e1d75e2591ea20f196c43
Student <example@example.com>
2019-08-07 11:35:02 -0700

    addec category functionality

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -3,6 +3,7 @@ using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -17,11 +18,12 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
+      List<Item> model = _db.Items.Include(items => items.Category).ToList();
       return View(model);
     }
     public ActionResult Create()
 {
+    ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
     return View();
 }
 
@@ -41,6 +43,7 @@ public ActionResult Details(int id)
 public ActionResult Edit(int id)
 {
     var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
     return View(thisItem);
 }
 
--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -5,6 +5,7 @@ namespace ToDoList.Models
   public class ToDoListContext : DbContext
   {
     public DbSet<Item> Items { get; set; }
+    public virtual DbSet<Category> Categories { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
   }
